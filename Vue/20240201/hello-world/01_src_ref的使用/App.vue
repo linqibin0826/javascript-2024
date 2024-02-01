448cc9ddf5d10fe804f11db3c530fac92b1c3976
linqibin <1214219989@qq.com>
2024-02-02 00:15:43 +0800

    vue nested components

---
+++ Vue/20240201/hello-world/01_src_ref的使用/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <School ref="school"></School>
+    <button @click="showInfoApp()">显示信息</button>
+  </div>
+</template>
+
+<script>
+import School from "@/components/School.vue";
+
+export default {
+  name: 'App',
+  components: {
+    School
+  },
+  methods: {
+    showInfoApp() {
+      console.log(this.$refs);
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ Vue/20240201/hello-world/01_src_ref的使用/components/School.vue
@@ -0,0 +1,28 @@
+<script>
+  export default {
+    name: 'School',
+    data() {
+      return {
+        name: '私立一中',
+        address: 'putian'
+      }
+    },
+    methods: {
+      showInfo() {
+        console.log(this.$refs);
+      }
+    }
+  }
+</script>
+
+<template>
+  <div>
+    <h2 ref="school-name">学校名称: {{ name }}</h2>
+    <h2 ref="school-address">学校地址: {{ address }}</h2>
+    <button @click="showInfo()">显示信息</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
